326513d0c1ba140c57bcd44350db186e61e7c832
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-06-11 15:23:41 +0800

    Initial design for Countries and Rates
    Develop code to be able:
    1. Push new countries and rate
    2. Delete selected countries and rate
    3. Add and drop checkboxes for countries

--- resources/js/Pages/Onclickcpm.vue
+++ resources/js/Pages/Onclickcpm.vue
@@ -1,121 +1,92 @@
 <script setup>
 import SidebarLayout from "@/Layouts/SidebarLayout.vue";
-import { useForm } from "@inertiajs/vue3";
-import {ref, watch} from "vue";
+import {router} from '@inertiajs/vue3'
+import {ref} from "vue";
 
-defineProps({
-    propads_timetable: Object,
-    propads_os_version: Object,
-    propads_os_type: Object,
-    propads_os: Object,
-    propads_device_type: Object,
-    propads_device: Object,
-    propads_browser: Object,
-    propads_zone: Object,
-    propads_connection: Object,
-    propads_mobile_isp: Object,
-    propads_proxy: Object,
-    propads_language: Object,
-    propads_audience: Object,
-    propads_traffic_categories: Object,
-});
+const givenCountries = [
+    {name:'United States',value:'us'},
+    {name:'Italy',value:'it'},
+    {name:'India',value:'in'},
+]
 
-//test countries
-const listCountries = ref([])
-//const count = ref(0)
+const totalCountries = ref([
+    {countries:['us','it'], amount:1},
+    {countries:['in'], amount:1}
+])
 
+const defaultCountries = ref([])
 
+function submit(){
 
-const isOpen = ref(false);
+    router.post('/onclick-cpm-create', {
+        countries:totalCountries
 
+    })
 
-const form = useForm({
-    name: null,
-    target_url:null,
-    is_adblock_buy:null,
-    countries:ref([]),
-    rates:null,
-    user_activity:ref([]),
-    remember: false,
+}
 
-});
 </script>
 
 <template>
     <SidebarLayout>
         <template #content>
             <div>
-                OnClick CPM
-                <p>Checked names: <pre>{{ form.name }}</pre></p>
-                <p>Checked target url: <pre>{{ form.target_url }}</pre></p>
-                <p>Checked adblock: <pre>{{ form.is_adblock_buy }}</pre></p>
-                <p>Checked countries: <pre>{{ form.countries }}</pre></p>
-                <p>Checked rates: <pre>{{ form.rates }}</pre></p>
 
-                <form @submit.prevent="form.post('/onclick-cpm-create')">
-                    <!-- name -->
-                    <input
-                        type="text"
-                        v-model="form.name"
-                        placeholder="name"
-                    >
-                    <br>
-                    <!-- target url -->
-                    <input
-                        type="text"
-                        v-model="form.target_url"
-                        placeholder="target url"
-                    >
-                    <br>
-                    <!-- adblock buy -->
-                    <input
-                        type="checkbox"
-                        v-model="form.is_adblock_buy"
-                        true-value=1
-                        false-value=null
-                    >
-                    <br>
-                    <!-- Countries -->
-                    <li v-for="item in propads_device_type.result">
-                        <input
-                            type="checkbox"
-                            v-model="form.countries"
-                            :value="item.value"
-                        >
-                        <label>{{item.value}}</label>
-                    </li>
-                    <br>
-                    <!-- user activity -->
-                    <input
-                        type="checkbox"
-                        value="1"
-                        v-model="form.user_activity"
-                    >
-                    <br>
-                    <!-- Rates -->
-                    <input
-                        type="text"
-                        v-model="form.rates"
-                        placeholder="rates"
+<!--                <pre>{{totalCountries[0]}}</pre>-->
 
-                    >
-                    <br>
-                    <input
-                        type="checkbox"
-                        value="2"
-                        v-model="form.user_activity"
-                    >
-                    <input
-                        type="checkbox"
-                        value="3"
-                        v-model="form.user_activity"
-                    >
-                    <br>
-                    <button
-                        type="submit"
-                        :disabled="form.processing"
-                    >Create</button>
+                <!--Loop every countries and rate -->
+
+                    <div v-for="(list,index) in totalCountries">
+                        <div>
+                            {{list}}
+                        </div>
+                        <div>
+                            <input
+                                type="checkbox"
+                                value="us"
+                                v-model="totalCountries[index].countries"
+                            >
+                            <label>USA</label>
+                            <br>
+
+                            <input
+                                type="checkbox"
+                                value="it"
+                                v-model="totalCountries[index].countries"
+                            >
+                            <label>Italy</label>
+                            <br>
+
+                            <input
+                                type="checkbox"
+                                value="in"
+                                v-model="totalCountries[index].countries"
+                            >
+                            <label>India</label>
+                            <br>
+
+                            <button
+                                type="button"
+                                @click="totalCountries.splice(index,1)"
+                            >Delete</button>
+                        </div>
+                    </div>
+
+
+
+                <button
+                    type="button"
+                    @click="totalCountries.push({countries:defaultCountries,amount:0})"
+                >
+                    Add rate
+                </button>
+
+                <br>
+
+                <form @submit.prevent="submit">
+                    <button type="submit">Submit</button>
                 </form>
+
             </div>
         </template>
     </SidebarLayout>
