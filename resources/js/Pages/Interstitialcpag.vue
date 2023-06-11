187c5786a8577279e8e7f2313c0af317051c46a7
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-06-11 19:45:43 +0800

    Initial design for Countries and Rate
    1. A list can be push
    2. A list can be delete at desired index
    3. The countries checkboxes can changed dynamically inside the array

--- resources/js/Pages/Interstitialcpag.vue
+++ resources/js/Pages/Interstitialcpag.vue
@@ -1,35 +1,110 @@
 <script setup>
 import SidebarLayout from "@/Layouts/SidebarLayout.vue";
-import RadioButtonCards from "@/Components/RadioButtonCards.vue";
-import HorizontalLine from "@/Components/HorizontalLine.vue";
-import AddDeleteAlert from "@/Components/AddDeleteAlert.vue";
-import AlertTriangle from "@/Components/AlertTriangle.vue";
-import AlertInfo from "@/Components/AlertInfo.vue";
-import { useForm } from "@inertiajs/vue3";
-import { ref } from "vue";
+import {router} from '@inertiajs/vue3'
+import {ref} from "vue";
 
+const givenCountries = [
+    {name:'United States',value:'us'},
+    {name:'Italy',value:'it'},
+    {name:'India',value:'in'},
+]
 
-const form = useForm({
-    name: null,
-    direction: null,
-    rate_model: null,
-    target_url:null,
-    remember: false,
-});
+const totalCountries = ref([
+    {countries:['us','it'], amount:1},
+    {countries:['in'], amount:1}
+])
+
+const defaultCountries = ref([])
+
+function submit(){
+
+    router.post('/onclick-cpm-create', {
+        countries:totalCountries
+
+    })
+
+}
+
 </script>
 
 <template>
     <SidebarLayout>
         <template #content>
             <div>
-                Interstitial CPA Goal
-                <form @submit.prevent="form.post('/interstitial-cpag-create')">
-                    <!-- name -->
-                    <input type="text" v-model="form.name">
-                    <button type="submit" :disabled="form.processing">Create</button>
+
+                <!--                <pre>{{totalCountries[0]}}</pre>-->
+
+                <!--Loop every countries and rate -->
+
+                <div v-for="(list,index) in totalCountries">
+
+                    <!--Display a list of countries-->
+                    <div>
+                        {{list.countries}}
+                    </div>
+
+                    <!--Display amount-->
+                    <div>
+                        {{list.amount}}
+                    </div>
+
+                    <!--Checkboxes for countries-->
+                    <div>
+                        <input
+                            type="checkbox"
+                            value="us"
+                            v-model="totalCountries[index].countries"
+                        >
+                        <label>USA</label>
+                        <br>
+
+                        <input
+                            type="checkbox"
+                            value="it"
+                            v-model="totalCountries[index].countries"
+                        >
+                        <label>Italy</label>
+                        <br>
+
+                        <input
+                            type="checkbox"
+                            value="in"
+                            v-model="totalCountries[index].countries"
+                        >
+                        <label>India</label>
+                        <br>
+
+                        <input
+                            type="text"
+                            v-model="totalCountries[index].amount"
+                        >
+                        <label>Amount</label>
+                        <br>
+
+                        <!--Delete a list-->
+                        <button
+                            type="button"
+                            @click="totalCountries.splice(index,1)"
+                        >Delete</button>
+                    </div>
+                </div>
+
+
+
+                <button
+                    type="button"
+                    @click="totalCountries.push({countries:defaultCountries,amount:0})"
+                >
+                    Add rate
+                </button>
+
+                <br>
+
+                <form @submit.prevent="submit">
+                    <button type="submit">Submit</button>
                 </form>
+
             </div>
         </template>
     </SidebarLayout>
 </template>
-
