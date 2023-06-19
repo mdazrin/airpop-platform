c6b1805dba40cebaa4dd81a7e65c35e31360b366
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-06-19 22:41:35 +0800

    Multiformat Campaign
    Develop UI for multiformat campaign

--- resources/js/Pages/Onclickcpag.vue
+++ resources/js/Pages/Onclickcpag.vue
@@ -6,7 +6,8 @@ import InputField from "@/Components/InputField.vue";
 import { useForm } from "@inertiajs/vue3";
 import { ref } from "vue";
 
-const advertFormat = ref()
+//advert format render condition
+const advertFormat = ref('onclick')
 
 
 const isOpen = ref(false);
@@ -136,6 +137,28 @@ const form = useForm({
                             </div>
                         </div>
 
+                    </div>
+
+                <!-- Multiformat Campaign-->
+                    <div class="mb-6">
+                        Multiformat campaign
+                        <div class="flex items-center mr-4">
+                            <label class="relative inline-flex items-center cursor-pointer">
+                                <input
+                                    type="checkbox"
+                                    value=""
+                                    class="sr-only peer"
+                                    checked>
+
+                                <div class="w-11 h-6 bg-gray-200 rounded-full peer
+                                peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700
+                                peer-checked:after:translate-x-full
+                                peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white
+                                after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600
+                                peer-checked:bg-blue-600"></div>
+                                <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Interstitial</span>
+                            </label>
+                        </div>
                     </div>
 
                 <!--Onclick Multiformat-->
