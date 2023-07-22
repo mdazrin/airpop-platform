fdec70049d901004f3e465b6f4d977f99ad5d9f8
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-07-22 10:05:53 +0800

    Initial design statistic page
    Develop using two APIs. First one to get the campaign Id's and second one for detailed statistics data for the requested campaigns
    
    1. GET/adv/campaigns
    2. POST/adv/statistics

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -15,6 +15,23 @@ class CampaignController extends Controller
             'is_archived'=>0
         ]);
 
+        $statistics = Http::withToken('b616d04fe21127a046c5fcf4024106dadef4792d9e7a889a')
+            ->post('https://ssp-api.propellerads.com/v5/adv/statistics',[
+                'group_by' => [
+                    'campaign_id'
+
+                ],
+                'day_from' => "2023-05-28 00:00:00",
+                'day_to' => "2023-06-02 23:59:59",
+                'campaign_id'=>[
+                    7007572,
+                    7006194
+
+                ]
+            ]);
+
+        dd($statistics->json());
+
         return Inertia::render('Campaign', [
             'campaign' => $response->json()
         ]);
