3b5c2d2283e3dcbe13c5cec21a0e180bfa2a454e
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-10-29 14:43:03 +0800

    Create CountriesRate component

--- resources/js/Pages/Onclickcpag.vue
+++ resources/js/Pages/Onclickcpag.vue
@@ -1,5 +1,6 @@
 <script setup>
 import SidebarLayout from "@/Layouts/SidebarLayout.vue";
+import CountriesRate from "@/Components/CampaignComponents/CountriesRate.vue";
 import Datepicker from 'vue3-datepicker'
 import {router} from "@inertiajs/vue3";
 import {ref} from "vue";
@@ -20,13 +21,7 @@ console.log(year1>=year2)
 const onclickMultiFormat = ref('onclick')
 const campaignDays = ref('Monday')
 
-//countries pool
-const countriesPool = ref([
-    {name:'USA',countryValue:'us', checkedValue:false},
-    {name:'India',countryValue:'in', checkedValue:false},
-    {name:'Italy',countryValue:'it', checkedValue:false}
 
-])
 
 //platform
 const platform = ref([
@@ -49,99 +44,6 @@ const desktop_os = ref ([
     {label:'Other Mobile', value:'other_mobile'},
 
 ])
-
-
-//list of countries and their rate
-const countriesRate = ref ([
-    {countries:[],amount:1}
-])
-
-const countriesList = ref([])
-
-function addCountry(countryIndex,index){
-    let a = countriesPool.value[countryIndex].countryValue
-
-    //push into countries Rate
-    countriesRate.value[index].countries.push(a)
-
-    //push into countries list
-    countriesList.value.push(a)
-
-    //changed the checked value inside countries pool
-    countriesPool.value[countryIndex].checkedValue = true
-
-    //sort countries rate
-    countriesRate.value[index].countries.sort()
-    countriesList.value.sort()
-
-
-}
-
-function removeCountry(listIndex,countryIndex,countryList){
-
-    //remove a country from countries rate
-    countriesRate.value[listIndex].countries.splice(countryIndex,1)
-
-
-    // changed the value of selected country in countries
-    for(let list in countriesPool.value){
-        let a = countriesPool.value[list].countryValue
-        if(a === countryList){
-            countriesPool.value[list].checkedValue = false
-        }
-
-    }
-
-    //remove country from the list
-    for(let list in countriesList.value){
-
-        let a = countriesList.value[list]
-
-        if(a === countryList){
-            countriesList.value.splice(list,1)
-        }
-
-    }
-
-}
-
-function removeList(listIndex){
-
-
-    for(let list in countriesRate.value){
-
-        let a = Number(list)
-        let b = Number(listIndex)
-        // console.log(list)
-
-        if(a === b){
-
-            let c = countriesRate.value[list].countries
-
-             for(let item of c){
-
-                 for(let pool in countriesPool.value){
-
-                    let b = countriesPool.value[pool].countryValue
-                    if(b === item){
-                        countriesPool.value[pool].checkedValue = false
-                    }
-
-                 }
-             }
-        }
-
-
-
-    }
-
-    countriesRate.value.splice(listIndex,1)
-
-}
-
-function checkedList(){
-    return countriesRate.value.length
-}
 
 //Timetable
 const days = ref(['Mo', 'Tu', 'We', 'Th', 'Fr','Sa','Su']);
@@ -260,7 +162,7 @@ const form = ref({
 
 
 //final submission of data, cannot be mutated
-//from form v-model and other mutated variables
+//from form v-models and other mutated variables
 function submit(){
 
     router.post('/create-campaign-create',{
@@ -291,8 +193,6 @@ function submit(){
     })
 
 }
-
-const searchQuery = ref([])
 
 </script>
 
@@ -457,106 +357,6 @@ const searchQuery = ref([])
                             v-model="form.target_url" />
                     </div>
 
-                    <!--Countries and Rates-->
-                    <div>
-                        <label>Countries and Rate</label>
-                        <br>
-                        <div class="flex space-x-4">
-                            <div class="w-full flex flex-col">
-                                <label>Countries</label>
-                                <div v-for="(list,listIndex) in countriesRate" class="border rounded-lg flex">
-                                        <ul class="flex items-center">
-                                        <li v-for="(countryList,countryIndex) in list.countries">
-                                        <button
-                                        class="border rounded-lg px-2 bg-cyan-600 text-white "
-                                        type="button"
-                                        @click="removeCountry(listIndex,countryIndex,countryList)"
-                                        >
-                                            {{countryList}}
-                                        </button>
-                                        </li>
-                                        <input type="text" v-model="searchQuery" placeholder="Search...">
-                                        </ul>
-                                </div>
-                                <div v-if="isDropdownVisible" class="relative">
-                                    <div class="absolute right-0 mt-2 w-full bg-white border rounded-lg shadow-lg">
-                                        <input type="text" v-model="searchQuery" placeholder="Search..."
-                                               @click="showOptions()" @blur="hideOptions()" class="w-full">
-                                        <div>
-                                            <ul v-for="(countries,index) in countriesPool" class="border">
-                                                <li v-if="countries.checkedValue === false" class="w-full">
-                                                    <button
-                                                    class="w-full"
-                                                    type="button"
-                                                    @click="addCountry(index,listIndex)">
-                                                    {{countries.name}}
-                                                </button>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="flex flex-col">
-                                <label for="">CPA Goal, $</label>
-                                <input class="rounded-lg" type="number" min="0" step="0.001">
-                            </div>
-                        </div>
-
-                        <!--Array of lists-->
-                        <div
-                            class ="border mb-4"
-                            v-for="(list,listIndex) in countriesRate">
-
-                            <!--Array of countries and price-->
-                            <li v-for="(countryList,countryIndex) in list.countries">
-                                <button
-                                type="button"
-                                @click="removeCountry(listIndex,countryIndex,countryList)"
-                                >
-                                    {{countryList}}
-                                </button>
-
-                            </li>
-                            <label v-for="price in list.amount">
-                                {{price}}
-                            </label>
-
-                            <!--Insert value of countries-->
-                            <h1 v-for="(countries,index) in countriesPool">
-                                <h1 v-if="countries.checkedValue === false">
-                                    <button
-                                    type="button"
-                                    @click="addCountry(index,listIndex)">
-                                        <label>{{countries.name}}</label>
-                                    </button>
-                                </h1>
-                                <h1 v-else-if="countries.checkedValue === true">
-                                    <label></label>
-                                </h1>
-                                <br>
-                            </h1>
-
-                            <div v-if="checkedList() > 1">
-                                <button
-                                    type="button"
-                                    @click="removeList(listIndex)">
-                                    Remove List
-                                </button>
-                            </div>
-
-                        </div>
-                        <!--End of Array Lists-->
-                    </div>
-
-                    <div>
-                        <button
-                        type="button"
-                        @click="countriesRate.push({countries:[],amount:1})">
-                            Add Countries
-                        </button>
-                    </div>
-                    <br>
 
                     <!--Advertising Budget-->
                     <div class="mb-4">
@@ -792,7 +592,9 @@ const searchQuery = ref([])
 
                         </div>
                             <br>
-                        </div> -->
+                        </div>
+
+
 
                         <div class="mb-4">
                             <label for="browser">Browser</label>
@@ -854,7 +656,8 @@ const searchQuery = ref([])
                                 >
                             </div>
 
-                            Start Date
+                            <CountriesRate></CountriesRate>
+
                             <div>
                                 <label>Starting Date</label>
                                 <Datepicker v-model="startDate"
