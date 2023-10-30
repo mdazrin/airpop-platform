9925ec53c07b749ee28f0ce6a98abfcc92376515
Azrin Hadahi <mdazrin.programmer@gmail.com>
2023-10-30 19:27:16 +0800

    Change navbar color

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -60,7 +60,10 @@ const open = ref(false);
                                             >
                                                 <button
                                                     type="button"
-                                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                    class="inline-flex items-center px-3 py-2 border border-transparent
+                                                    text-sm leading-4 font-medium rounded-md text-white bg-gray-700
+                                                    hover:text-white-300 focus:outline-none transition ease-in-out
+                                                    duration-150"
                                                 >
                                                     {{
                                                         $page.props.auth.user
